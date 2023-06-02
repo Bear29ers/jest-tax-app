519938bce178bc4b5e36d48ecb304298d1fa367b
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-02 10:06:34 +0900

    feat: :sparkles: POSTでJSONボディを受け取る

--- tax-api-experiment/src/index.ts
+++ tax-api-experiment/src/index.ts
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (_, res) => {
   res.send('Hello World!');
 });
@@ -14,6 +16,11 @@ app.post('/check-post', (_, res) => {
 
 app.post('/check-status-code', (_, res) => {
   res.status(500).json({ message: 'Hello StatusCode!' });
+});
+
+app.post('/check-body', (req, res) => {
+  console.dir(req.body);
+  res.json({ message: 'Hello JSON body!' });
 });
 
 app.listen(port, () => {
