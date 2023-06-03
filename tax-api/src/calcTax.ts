fba67332db2eec9d1e84a1117fb935acf7353b82
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-03 17:59:05 +0900

    feat: :sparkles: テストに守られたまま実装を改善する

--- tax-api/src/calcTax.ts
+++ tax-api/src/calcTax.ts
@@ -1,4 +1,4 @@
-type calcRetirementIncomeDeductionInput = {
+type CalcRetirementIncomeDeductionInput = {
   // 勤続年数
   yearsOfService: number;
   // 障害者となったことに直接起因して退職したか
@@ -6,8 +6,9 @@ type calcRetirementIncomeDeductionInput = {
 };
 
 // 退職所得控除額
+/*
 export const calcRetirementIncomeDeduction = (
-  input: calcRetirementIncomeDeductionInput,
+  input: CalcRetirementIncomeDeductionInput,
 ): number => {
   let result = 0;
 
@@ -24,4 +25,24 @@ export const calcRetirementIncomeDeduction = (
   }
 
   return result;
+};
+*/
+
+// 退職所得控除額（リファクタリング）
+export const calcRetirementIncomeDeduction = ({
+  yearsOfService,
+  isDisability,
+}: CalcRetirementIncomeDeductionInput): number => {
+  let deduction = 0;
+  if (yearsOfService === 1) {
+    deduction = 800_000;
+  } else if (yearsOfService <= 19) {
+    deduction = 400_000 * yearsOfService;
+  } else {
+    deduction = 8_000_000 + 700_000 * (yearsOfService - 20);
+  }
+
+  if (isDisability) deduction += 1_000_000;
+
+  return deduction;
 };
