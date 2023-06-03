40174d81337e16d3d412abbe46c3b87f362e547f
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-03 17:53:44 +0900

    feat: :sparkles: テストファーストで安心して実装する

--- tax-api/src/calcTax.test.ts
+++ tax-api/src/calcTax.test.ts
@@ -1,5 +1,6 @@
 import { calcRetirementIncomeDeduction } from './calcTax';
 
+/*
 test('退職所得控除額', () => {
   const deduction = calcRetirementIncomeDeduction({
     // 勤続年数
@@ -8,4 +9,34 @@ test('退職所得控除額', () => {
     isDisability: false,
   });
   expect(deduction).toBe(800_000);
+});
+*/
+
+describe('退職所得控除額', () => {
+  test.each`
+    yearsOfService | isDisability | expected
+    ${1}           | ${false}     | ${800_000}
+    ${2}           | ${false}     | ${800_000}
+    ${3}           | ${false}     | ${1_200_000}
+    ${19}          | ${false}     | ${7_600_000}
+    ${20}          | ${false}     | ${8_000_000}
+    ${21}          | ${false}     | ${8_700_000}
+    ${30}          | ${false}     | ${15_000_000}
+    ${1}           | ${true}      | ${1_800_000}
+    ${2}           | ${true}      | ${1_800_000}
+    ${3}           | ${true}      | ${2_200_000}
+    ${19}          | ${true}      | ${8_600_000}
+    ${20}          | ${true}      | ${9_000_000}
+    ${21}          | ${true}      | ${9_700_000}
+    ${30}          | ${true}      | ${16_000_000}
+  `(
+    '$yearsOfService年 isDisability:$isDisability → $expected',
+    ({ yearsOfService, isDisability, expected }) => {
+      const deduction = calcRetirementIncomeDeduction({
+        yearsOfService, // 勤続年数
+        isDisability, // 障害者となったことに直接起因して退職したか
+      });
+      expect(deduction).toBe(expected);
+    },
+  );
 });
--- tax-api/src/calcTax.ts
+++ tax-api/src/calcTax.ts
@@ -8,6 +8,20 @@ type calcRetirementIncomeDeductionInput = {
 // 退職所得控除額
 export const calcRetirementIncomeDeduction = (
   input: calcRetirementIncomeDeductionInput,
-) => {
-  return 800_000;
+): number => {
+  let result = 0;
+
+  if (input.yearsOfService === 1) {
+    result = 800_000;
+  } else if (input.yearsOfService <= 19) {
+    result = 400_000 * input.yearsOfService;
+  } else {
+    result = 8_000_000 + 700_000 * (input.yearsOfService - 20);
+  }
+
+  if (input.isDisability) {
+    result += 1_000_000;
+  }
+
+  return result;
 };
