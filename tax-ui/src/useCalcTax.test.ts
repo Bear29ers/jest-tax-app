f3c02bd68c1f1f49a64785bd3c409cf23d485caf
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-20 23:22:30 +0900

    test: :green_heart: テストを記述してフックの動きを確認する

---
+++ tax-ui/src/useCalcTax.test.ts
@@ -0,0 +1,28 @@
+import { act, waitFor } from '@testing-library/react';
+
+import { renderHook } from './test-utils';
+import { useCalcTax } from './useCalcTax';
+
+describe('useCalcTax', () => {
+  test('所得税計算APIを呼び出せる', async () => {
+    // フックをレンダリング
+    const { result } = renderHook(() => useCalcTax());
+
+    // フックから返されたmutate関数を使用してAPIを呼び出す
+    act(() => {
+      result.current.mutate({
+        yearsOfService: 6,
+        isOfficer: false,
+        isDisability: false,
+        severancePay: 3_000_000,
+      });
+    });
+
+    // フックの結果が成功になるまで待つ
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    // ステータスコードとレスポンスボディのJSONデータを確認
+    expect(result.current.data?.status).toBe(200);
+    expect(await result.current.data?.json()).toStrictEqual({ tax: 15315 });
+  });
+});
