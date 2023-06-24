39b7453c821f0d05d6e4307a15fbfffd603daa4a
Yuta Okuma <y.dream.nest@icloud.com>
2023-06-24 20:56:08 +0900

    refactor: :bulb: 残すStoryを考える

--- tax-ui/src/Page.stories.tsx
+++ tax-ui/src/Page.stories.tsx
@@ -7,16 +7,16 @@ export default {
   component: Presentation,
 } as ComponentMeta<typeof Presentation>;
 
-export const Standard: ComponentStoryObj<typeof Presentation> = {
-  args: { tax: 10000 },
-};
+// export const Standard: ComponentStoryObj<typeof Presentation> = {
+//   args: { tax: 10000 },
+// };
 
-export const NoResult: ComponentStoryObj<typeof Presentation> = {
-  args: { tax: null },
-};
+// export const NoResult: ComponentStoryObj<typeof Presentation> = {
+//   args: { tax: null },
+// };
 
 export const ValidationError: ComponentStoryObj<typeof Presentation> = {
-  args: { tax: null },
+  args: { tax: null, calcStatus: 'before-calculation' },
   play: () => {
     userEvent.clear(screen.getByLabelText('勤続年数'));
     userEvent.clear(screen.getByLabelText('退職金'));
